79f5db84ac7ea4f690c1516cd642337d89d9c992
Pablo Mercado <32917233+Pablo497@users.noreply.github.com>
2024-08-05 19:08:46 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+let burger = document.getElementById('')
+let menu = document.getElementById('')
+
+menu.addEventListener('click', () => {
+  
+  if(){
+    menu.style.display = ""
+  } else{
+    menu.style.display = ""
+  }
+  
+})
